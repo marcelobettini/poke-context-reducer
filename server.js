918ac01404441e7bfdafc49e3e2bfbb430f6708a
unknown <marcelobettini@yahoo.com.ar>
2022-05-15 21:29:00 -0300

    path build to dist

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import path from 'path';
 const PORT = process.env.PORT || 5000
 if (process.env.NODE_ENV === 'production') {
     server.use(express.static('dist'))
-    server.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
-    })
+    // server.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+    // })
 }
 server.listen(PORT, (err) => console.log(err ? err : `server up at http://localhost:${PORT}`))
