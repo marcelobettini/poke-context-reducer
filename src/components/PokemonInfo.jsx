d176e44d20272078599afd2bc0fb1be455dc511d
unknown <marcelobettini@yahoo.com.ar>
2022-05-18 13:44:59 -0300

    Rank added to info component

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -2,14 +2,18 @@ import PokemonContext from "../PokemonContext";
 import { Card, CardContent, CardActions, Typography, Button } from "@mui/material";
 import { useContext } from "react";
 const PokemonInfo = () => {
+    let power = 0
     const { state: { selectedPokemon }, dispatch } = useContext(PokemonContext)
     return selectedPokemon && (
-        <Card sx={{ height: 350, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
+        <Card sx={{ border: '1px solid lightGray', height: 310, minWidth: 250, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }} >
             <CardContent>
-                <Typography variant="h5" gutterBottom>{selectedPokemon.name.english}</Typography>
-                {Object.keys(selectedPokemon.base).map((key) => (
-                    <Typography key={key}>{key} : {selectedPokemon.base[key]}</Typography>
-                ))}
+                <Typography variant="h5" gutterBottom sx={{ textAlign: 'center' }}>{selectedPokemon.name.english}</Typography>
+                {Object.keys(selectedPokemon.base).map((key) => {
+                    power += +selectedPokemon.base[key]
+                    return <Typography key={key}>{key} : {selectedPokemon.base[key]}</Typography>
+                })}
+                <Typography variant="h6" sx={{ textAlign: 'center' }}>Rank: {power}</Typography>
+
             </CardContent>
             <CardActions>
                 <Button variant="contained" color="secondary" onClick={() => dispatch({
@@ -17,7 +21,7 @@ const PokemonInfo = () => {
                     payload: null
                 })}>dismiss</Button>
             </CardActions>
-        </Card>
+        </Card >
     )
 }
 export default PokemonInfo
